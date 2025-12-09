87311250638e240cc048509ad5894f39eb42eb23
Matthew Cacutt <matthew.cacutt@gmail.com>
2025-12-09 11:35:24 +0000

    fix mkui

--- TygerMemory3/MKUI.cpp
+++ TygerMemory3/MKUI.cpp
@@ -3,14 +3,8 @@
 uintptr_t MKUI::GetBaseUI()
 {
     uintptr_t address = Core::moduleBase + 0x4c6024;
+    address = *(uintptr_t*)(address + 0x64);
     return address;
-}
-
-UIElementStruct* MKUI::GetMainMenu()
-{
-    uintptr_t uiManager = *(uintptr_t*)(GetBaseUI());
-    UIElementStruct* mainMenu = *(UIElementStruct**)(uiManager + 0x4FC);
-    return mainMenu;
 }
 
 UIElementStruct* MKUI::FindChildElementByName(UIElementStruct* parent, const char* childTypeName)
@@ -31,4 +25,59 @@ UIElementStruct* MKUI::FindChildElementByName(UIElementStruct* parent, const cha
 
     // Not found
     return nullptr;
+}
+
+UIElementStruct* MKUI::FindElementByName(const char* elementName)
+{
+    uintptr_t baseUI = GetBaseUI();
+    return FindElementRecursive(reinterpret_cast<UIElementStruct*>(baseUI), elementName);
+}
+
+UIElementStruct* MKUI::FindElementRecursive(UIElementStruct* element, const char* name)
+{
+    if (element == nullptr || name == nullptr)
+        return nullptr;
+
+    if (element->TypeName && strcmp(element->TypeName, name) == 0)
+        return element;
+
+    UIElementStruct* child = element->firstChildUiElement;
+
+    for (int i = 0; i < element->numChildElements && child != nullptr; ++i)
+    {
+        UIElementStruct* found = FindElementRecursive(child, name);
+        if (found)
+            return found;
+        child = child->nextUiElementInList;
+    }
+
+    return nullptr;
+}
+
+std::string MKUI::GetUITreeString()
+{
+    uintptr_t baseUI = GetBaseUI();
+    UIElementStruct* root = reinterpret_cast<UIElementStruct*>(baseUI);
+
+    std::string result;
+    BuildUITreeString(root, result, 0);
+    return result;
+}
+
+void MKUI::BuildUITreeString(UIElementStruct* element, std::string& out, int depth)
+{
+    if (!element)
+        return;
+    out.append(std::string(depth * 2, ' '));
+    if (element->TypeName)
+        out.append(element->TypeName);
+    else
+        out.append("<null>");
+    out.append("\n");
+    UIElementStruct* child = element->firstChildUiElement;
+    for (int i = 0; i < element->numChildElements && child != nullptr; ++i)
+    {
+        BuildUITreeString(child, out, depth + 1);
+        child = child->nextUiElementInList;
+    }
 }
--- TygerMemory3/MKUI.h
+++ TygerMemory3/MKUI.h
@@ -18,31 +18,33 @@ struct UIElementStruct {
 	bool enabled;
 	bool currentlySelected;
 	bool interactable;
+	int unk18;
+	int unk1c;
+
 	float Blue;
 	float Red;
-
 	float Green;
 	float Alpha;
-	char unk28[8];
 
-	char unk30[4];
-	float unk34;
-	char unk38[8];
+	char unk30[0xC];
+	float unk3C;
 
-	char unk40[8];
+	char unk40[0x10];
 	float size;
-	char unk4c[4];
+	char unk54[0xC];
 
-	char unk50[8];
-	int unk58;
+	char unk60[4];
 	UIElementStruct* firstChildUiElement;
 	int numChildElements;
 };
 
 class TYGERMEM2 MKUI {
-
+private:
+	static UIElementStruct* FindElementRecursive(UIElementStruct* element, const char* name);
+	static void BuildUITreeString(UIElementStruct* element, std::string& out, int depth);
 public:
 	static uintptr_t GetBaseUI();
-	static UIElementStruct* GetMainMenu();
+	static UIElementStruct* FindElementByName(const char* elementName);
 	static UIElementStruct* FindChildElementByName(UIElementStruct* parent, const char* childname);
+	static std::string GetUITreeString();
 };
